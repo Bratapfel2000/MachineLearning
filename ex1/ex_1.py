958c79e12e65dd3d4b77ab94afbf1517fab86b26
Bratapfel2000 <43928387+Bratapfel2000@users.noreply.github.com>
2020-05-30 16:44:11 +0200

    Add files via upload

--- ex1/ex_1.py
+++ ex1/ex_1.py
@@ -6,7 +6,9 @@ import numpy as np
 data_1, data_2 = np.loadtxt('ex1data1.txt',
                  unpack = True,
                  delimiter = ",")
-plt.plot(data_1, data_2 , 'o')
+plt.plot(data_1, data_2 , '+', label='Training Data')
+plt.legend(loc='best')
+
 plt.xlabel("Population of City in 10,000s")
 plt.ylabel('Profit in $10,000s')
 
@@ -18,7 +20,6 @@ a = np.array([data_2])   # load in data in array
 y = a.T                 #transpose it to for later addition
 
 ##print("a.shape",a.shape)
-##plt.show()
 
 
 m = len(y)
@@ -68,19 +69,59 @@ print(" ")
 
 print("With theta = [-1 ; 2]")
 print("Expected cost value (approx) 54.24")
-print("J =", end = "")
+print("J =", end = " ")
 
 theta_2 = theta
 theta_2[0] = -1
 theta_2[1] = 2
 
 print(computeCost(X, y, theta_2))
+print(" ")
 
+
+#X[:,:2]
 
 
 def gradientDescent(X, y, theta, alpha, num_iters):
     m = len(y)
-    J_history = np.zeros((num_iters,1))    
+    J_history = np.zeros((num_iters,1))
+
+    for i in range(num_iters):
+        x_theta = np.matmul(X, theta)
+        x_theta_y = x_theta - y
+##        temp0 = theta[0] - ((alpha/m)*sum(((X * theta) - y)))    
+        temp0 = theta[0] - ((alpha/m)*sum(x_theta_y))
+##        temp1 = theta[1] - ((alpha/m)*sum(((X * theta) - y).*X(:,2)));
+        temp1_term_0 = (alpha/m)
+        X_term = X[ : , 1] 
+        X_term_dim = np.array([X_term]) #create (1,x) array, transpose to get (x,1). should be easier.
+        X_ready = X_term_dim.T 
+        temp1_term_1 = sum(np.multiply(x_theta_y, X_ready))
+        temp1 = theta[1] - temp1_term_0*temp1_term_1     
+        
+
+        theta[0] = temp0
+        theta[1] = temp1
+##        print(temp0,temp1)
+        J_history[i] = computeCost(X, y, theta)
+##    print(">>>", theta[1],theta[0])
+    return theta[0],theta[1]
+
+theta = np.zeros((2,1))
+
+theta = gradientDescent(X, y, theta, alpha, iterations)
+print("Expected theta values (approx)");
+print(" -3.6303   1.1664");
+print("Theta >>",theta)
+        
+
+
+X_term = X[ : , 1]
+X_term_dim = np.array([X_term])#create (1,x) array, transpose to get (x,1). should be easier. 
+X_ready = X_term_dim.T 
+x_theta = np.matmul(X, theta)
+plt.plot(X_ready,x_theta , '-', label='Fit')
+plt.legend(loc='best')
 
 
 
@@ -97,6 +138,5 @@ def gradientDescent(X, y, theta, alpha, num_iters):
 
 
 
-
-
+plt.show()
 
